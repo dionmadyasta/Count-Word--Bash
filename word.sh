d875c1f3de68f90f5ef3d06961bdd06545e67bbf
dionmadyasta <dion.madyasta@gmail.com>
2020-09-10 13:12:50 +0700

    Update word.sh

--- word.sh
+++ word.sh
@@ -4,7 +4,7 @@ file="/mnt/d/kata.txt" #my local directory to store word.txt
 while IFS= read line
 do
 
-        echo -n "That words start with :" \"${line:2:1}\" | tr '[a-z]' '[A-Z]';
+        echo -n "That words start with" \"${line:2:1}\" | tr '[a-z]' '[A-Z]';
         echo " or "\"${line:2:1}\" | tr '[A-Z]' '[a-z]';
 
 if [[ "$line" -gt 5 ]] ; then
