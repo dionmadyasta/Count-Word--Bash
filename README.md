e4277678778dba0556b3a0cc45b7ab19374b516d
dionmadyasta <dion.madyasta@gmail.com>
2020-09-10 13:09:10 +0700

    all

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# DGIT-test
+### DGIT DevOps Test-DevOps coding assessment
+
+Dionyius Dewaji Madyasta, has applied as devops
+
+File : 
+
+1. word.sh
+4. kata.txt
+
+run : bash word.sh
---
+++ word.sh
@@ -0,0 +1,17 @@
+#!/bin/ksh
+
+file="/mnt/d/kata.txt" #my local directory to store word.txt
+while IFS= read line
+do
+
+        echo -n "That words start with :" \"${line:2:1}\" | tr '[a-z]' '[A-Z]';
+        echo " or "\"${line:2:1}\" | tr '[A-Z]' '[a-z]';
+
+if [[ "$line" -gt 5 ]] ; then
+    echo ""
+    exit 1
+else
+    echo "All the words longer than 5 characters"
+    exit 0
+fi
+done <"$file"
